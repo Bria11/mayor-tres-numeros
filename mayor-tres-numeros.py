924aa8e82af0ee1065c83439405df3282572723b
LAB_J205 <labj205@continental.edu.pe>
2025-03-28 11:49:29 -0500

    sE MODIFICO EL CODIGO CON EL EMFOQUE DE POO

--- mayor-tres-numeros.py
+++ mayor-tres-numeros.py
@@ -1,26 +1,49 @@
-"""Determina el mayor de tres numeros ingresados por el teclado
 """
 
-def intercambiar_valores(numero1,numero2):
-    temporal = numero1
-    numero1 = numero2
-    numero2 = temporal
-    return numero1,numero2
+"""
+class OrdenadarTresNumeros:
 
-numero1=int(input("Ingresa el primer numero: "))
-numero2=int(input("Ingresa el segundo numero: "))
-numero3=int(input("Ingresa el tercer numero: "))
+    def __init__(self, numero1, numero2, numero3):  # Corregido: __int__ por __init__
+        self.numero1 = numero1
+        self.numero2 = numero2
+        self.numero3 = numero3
 
-if numero1>numero2:
-    numero1,numero2 = intercambiar_valores(numero1,numero2)
+    def intercambiar_valores(self, numero1, numero2):
+        temporal = numero1
+        numero1 = numero2
+        numero2 = temporal
+        return numero1, numero2
 
+    def ingresar_numeros(self):
+        self.numero1 = int(input("Ingrese el primer numero : "))
+        self.numero2 = int(input("Ingrese el segundo numero : "))  # Corregido: "primer" por "segundo"
+        self.numero3 = int(input("Ingrese el tercer numero : "))   # Corregido: "primer" por "tercer"
 
-if numero2>numero3:
-    numero2, numero3 = intercambiar_valores(numero2, numero3)
+    def ordenar_numeros(self):
+        if self.numero1 > self.numero2:
+            self.numero1, self.numero2 = self.intercambiar_valores(self.numero1, self.numero2)  # Añadido self.
 
+        if self.numero2 > self.numero3:
+            self.numero2, self.numero3 = self.intercambiar_valores(self.numero2, self.numero3)  # Añadido self.
 
-if numero1>numero2:
-    numero1, numero2 = intercambiar_valores(numero1, numero2)
+        if self.numero1 > self.numero2:
+            self.numero1, self.numero2 = self.intercambiar_valores(self.numero1, self.numero2)  # Añadido self.
 
-print(f"numeros ordenados: {numero1},{numero2},{numero3}")
-print(f"El mayor es {numero3}")
+    def imprimir_numeros(self):
+        print(f"Numeros ordenados: {self.numero1}, {self.numero2}, {self.numero3}")
+
+if __name__ == "__main__":
+    numero1 = 5
+    numero2 = 10
+    numero3 = 1
+    numeros = OrdenadarTresNumeros(numero1, numero2, numero3)
+    print("\n Numeros desordenados\n")
+    numeros.imprimir_numeros()
+    print("\n Numeros ordenados\n")
+    numeros.ordenar_numeros()
+    numeros.imprimir_numeros()
+
+    numeros.ingresar_numeros()
+    print("\n Numeros ordenados\n")
+    numeros.ordenar_numeros()
+    numeros.imprimir_numeros()
